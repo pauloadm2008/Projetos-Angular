a19604fd5803e0a1bac43ccd9508218628da53ae
pauloadm2008 <pauloadm2008@gmail.com>
2023-04-19 21:28:07 -0300

    feat: Porjeto Angular com componentes prime ng @0.5

--- projeto-tr-1/src/app/app.module.ts
+++ projeto-tr-1/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, BrowserAnimationsModule, AccordionModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
