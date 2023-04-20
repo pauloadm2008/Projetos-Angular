b8f599a26c9996cf198894a7146aa063c63e0408
pauloadm2008 <pauloadm2008@gmail.com>
2023-04-19 22:20:50 -0300

    feat:Aplicação do componente alert @1

--- projeto-tr-1/src/app/app.component.ts
+++ projeto-tr-1/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Message } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'projeto-tr-1';
+  msgs: Message[] = [
+    {
+      severity: 'success',
+      summary: 'GeeksforGeeks',
+      detail: 'This is a message',
+    },
+  ];
+  hide() {
+    this.msgs = [];
+  }
+  ngOnInit() {}
 }
--- projeto-tr-1/src/app/app.module.ts
+++ projeto-tr-1/src/app/app.module.ts
@@ -5,8 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AccordionModule } from 'primeng/accordion';
 
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AccordionModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    MessagesModule,
+    MessageModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
