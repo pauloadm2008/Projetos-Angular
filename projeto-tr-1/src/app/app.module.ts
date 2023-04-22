56a4b671d716915096aad99c0eed82d54bb155e8
pauloadm2008 <pauloadm2008@gmail.com>
2023-04-22 14:47:35 -0300

    feat: implantação do componente primeng carrocel @0.5

--- projeto-tr-1/src/app/app.module.ts
+++ projeto-tr-1/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { CarouselModule } from 'primeng/carousel';
 import { AppComponent } from './app.component';
 import { AccordionModule } from 'primeng/accordion';
-
+import { ButtonModule } from 'primeng/button';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
-
+import { TagModule } from 'primeng/tag';
 @NgModule({
   imports: [
     BrowserModule,
+    TagModule,
+    ButtonModule,
+    CarouselModule,
     BrowserAnimationsModule,
     AccordionModule,
     MessagesModule,
